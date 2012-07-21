8da4c50e8b7cdf68c773ab678ca2c49ef29d01c7
Cosmin Radoi <cosmin.radoi@gmail.com>
2012-07-21 18:30:17 -0500

    first commit

---
+++ src/util/debug.scala
@@ -0,0 +1,17 @@
+package util
+
+object debug {
+  var active = false
+  def activate = active = true
+
+  private var detailedContexts = false
+  //  def activateDetailedContexts = detailedContexts = true
+  def detailContexts = detailedContexts && active
+
+  def apply(note: Any) = { display(note) }
+
+  def display(note: Any) {
+    if (active)
+      println(note)
+  }
+}
