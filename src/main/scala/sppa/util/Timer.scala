3b058afe14bef6952b1f95c9f2e085794c8519d0
Cosmin Radoi <cosmin.radoi@gmail.com>
2012-12-14 18:34:57 -0600

    add Timer class

---
+++ src/main/scala/sppa/util/Timer.scala
@@ -0,0 +1,24 @@
+package sppa.util
+
+import java.util.TimerTask
+
+object Timer {
+  def apply(delay: Long, period: Long)(op: => Unit) = {
+    val timer = new java.util.Timer()
+    timer.schedule(new TimerTask() {
+      def run() {
+        op
+      }
+    }, delay, period)
+    timer
+  }
+  def apply(delay: Long)(op: => Unit) = {
+    val timer = new java.util.Timer()
+    timer.schedule(new TimerTask() {
+      def run() {
+        op
+      }
+    }, delay)
+    timer
+  }
+}
